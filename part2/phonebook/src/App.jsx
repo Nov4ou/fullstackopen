f6e5443023e24ea5f0d2f2d10d448db7e48eab60
Nov4ou <nov4ou@outlook.com>
2025-03-31 16:34:45 +0800

    feat: complete exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Filter = ({ filter, handleFilterChange }) => (
   <div>
@@ -26,7 +27,7 @@ const PersonForm = ({
   </form>
 )
 
-const Person = ({filteredPersons}) => (
+const Person = ({ filteredPersons }) => (
   <div>
     {filteredPersons.map(person =>
       <p>{person.name} {person.number}</p>
@@ -35,15 +36,21 @@ const Person = ({filteredPersons}) => (
 )
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setNewFilter] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(respnose => {
+        console.log('promise fulfilled')
+        setPersons(respnose.data)
+      })
+  }, [])
+  console.log('render', persons.length, 'persons')
 
   const isNameExists = (name) => {
     return persons.some((persons) => persons.name === name)
