db8c5bf3db3169b941cc02123b15146ac2405822
Xinghang Chen <nov4ou@root-delight-1.localdomain>
2025-04-10 20:28:50 -0700

    uibuild

--- part3/deploy.sh
+++ part3/deploy.sh
@@ -6,7 +6,7 @@ cd phonebook_backend
 echo "=== Deploy started at $(date) ===" >> ../deploy.log
 
 git pull origin deploy >> ../deploy.log 2>&1
-npm install >> ../../deploy.log 2>&1
+npm install >> ../deploy.log 2>&1
 pm2 restart backend >> ../deploy.log 2>&1
 
 echo "=== Deploy finished at $(date) ===" >> ../deploy.log
