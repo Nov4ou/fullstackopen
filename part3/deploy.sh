f918a444e01f014135bcd99de14f6b2ad308ac27
Nov4ou <nov4ou@outlook.com>
2025-04-14 15:47:01 +0800

    chore: change deploy branch from deploy to main

--- part3/deploy.sh
+++ part3/deploy.sh
@@ -5,7 +5,7 @@ cd phonebook_backend
 
 echo "=== Deploy started at $(date) ===" >> ../deploy.log
 
-git pull origin deploy >> ../deploy.log 2>&1
+git pull origin main >> ../deploy.log 2>&1
 npm install >> ../deploy.log 2>&1
 pm2 restart backend >> ../deploy.log 2>&1
 
