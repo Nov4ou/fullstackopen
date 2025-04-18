eff663d6764fe97ee01109809e2a7fa1e6c0b24a
Nov4ou <nov4ou@outlook.com>
2025-04-18 16:58:15 +0800

    feat: complete exercise 3.17

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -107,6 +107,25 @@ app.post('/api/persons', (request, response) => {
   })
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
+
+  Person.findById(request.params.id)
+    .then(person => {
+      if (!person) {
+        return response.status(404).end()
+      }
+
+      person.name = name
+      person.number = number
+
+      return person.save().then(updatedPerson => {
+        response.json(updatedPerson)
+      })
+    })
+    .catch(error => next(error))
+})
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
@@ -115,13 +134,13 @@ app.use(unknownEndpoint)
 
 const PORT = process.env.PORT
 app.listen(PORT)
-console.log(`Server running on port ${PORT}`) 
+console.log(`Server running on port ${PORT}`)
 
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  }
   next(error)
 }
 
