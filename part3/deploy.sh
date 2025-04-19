b99c21db68e1dd06e2f2edf1b407435c78363ce5
Nov4ou <nov4ou@outlook.com>
2025-04-19 17:46:22 +0800

    fix:  update deploy.sh to manually rebuild frontend

--- part3/deploy.sh
+++ part3/deploy.sh
@@ -6,8 +6,12 @@ cd phonebook_backend
 echo "=== Deploy started at $(date) ===" >> ../deploy.log
 
 git pull origin main >> ../deploy.log 2>&1
+
 npm install >> ../deploy.log 2>&1
-npm run build:ui
+rm -rf dist
+cd ../phonebook_frontend
+npm run build
+cp -r dist ../phonebook_backend/
 pm2 restart backend >> ../deploy.log 2>&1
 
 echo "=== Deploy finished at $(date) ===" >> ../deploy.log
