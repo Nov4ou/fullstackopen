9cd3ebb128d650312e43a58f5fcef02104168b8b
Nov4ou <nov4ou@outlook.com>
2025-04-22 17:04:50 +0800

    feat: complete exercise 4.2

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,18 +1,6 @@
-const express = require('express')
-const mongoose = require('mongoose')
+const app = require('./app')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-const blogsRouter = require('./controllers/blogs')
-const app = express()
-
-
-
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl)
-
-app.use(express.json())
-
-app.use('/api/blogs', blogsRouter)
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
