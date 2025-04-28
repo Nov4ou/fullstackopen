c3753b4767e713bd3b9626f19340ff14db91960e
Nov4ou <nov4ou@outlook.com>
2025-04-28 19:27:34 +0800

    feat: complete exercise 4.10

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -30,6 +30,28 @@ test('unique identifier is named id', async () => {
     assert.ok(blog.hasOwnProperty('id'))
   })
 })
+
+test('a vaild blog can be added', async () => {
+  const newBlog = {
+    title: 'CASPP',
+    author: 'Xinghang Chen',
+    url: 'https://nov4ou.github.io',
+    likes: 777,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+  const title = response.body.map(r => r.title)
+
+  assert.strictEqual(response.body.length, helper.initialblogs.length + 1)
+  assert(title.includes('CASPP'))
+})
+
 after(async () => {
   await mongoose.connection.close()
 })
