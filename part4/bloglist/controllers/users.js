b61dfaa41bfba331a99fa83245ea15669628bf1c
Nov4ou <nov4ou@outlook.com>
2025-05-08 15:36:15 +0800

    feat: complete exercise 4.17

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -3,7 +3,8 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({})
+  const users = await User
+    .find({}).populate('blogs', { url: 1, title: 1, author: 1 })
   response.json(users)
 })
 
@@ -14,8 +15,8 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: 'username or password missing' })
 
   if (username.length < 3 || password.length < 3)
-    return response.status(400).json({error: 'username and password must be at least 3 characters long'})
-  
+    return response.status(400).json({ error: 'username and password must be at least 3 characters long' })
+
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
 
