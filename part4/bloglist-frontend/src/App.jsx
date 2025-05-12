39305e8051808de6ab7b23d7096f05f3df9c950d
Nov4ou <nov4ou@outlook.com>
2025-05-12 19:56:48 +0800

    feat: complete exercise 5.4

--- part4/bloglist-frontend/src/App.jsx
+++ part4/bloglist-frontend/src/App.jsx
@@ -3,6 +3,17 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
 
+const Notification = ({ message }) => {
+  if (message === null)
+    return null
+
+  return (
+    <div className={message.type}>
+      {message.text}
+    </div>
+  )
+}
+
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('')
@@ -12,6 +23,9 @@ const App = () => {
   const [newTitle, setNewTitle] = useState('')
   const [newAuthor, setNewAuthor] = useState('')
   const [newUrl, setNewUrl] = useState('')
+
+  const [addMessage, setAddMessage] = useState({ text: null, type: '' })
+  const [loginMessage, setLoginMessage] = useState({ text: null, type: '' })
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -31,19 +45,27 @@ const App = () => {
   const handleLogin = async (event) => {
     event.preventDefault()
 
-    const user = await loginService.login({
-      username, password,
-    })
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
 
-    window.localStorage.setItem(
-      'loggedBlogappUser', JSON.stringify(user)
-    )
-    blogService.setToken(user.token)
-    setUser(user)
-    setUsername('')
-    setPassword('')
-    console.log('logging in with', username, password)
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+      console.log('logging in with', username, password)
+    } catch (error) {
+      setLoginMessage({ text: 'wrong username or password', type: 'error' })
+      setTimeout(() => {
+        setLoginMessage({ text: null, type: '' })
+      }, 5000)
+    }
   }
+
 
   const handleLogout = () => {
     window.localStorage.removeItem('loggedBlogappUser')
@@ -59,8 +81,12 @@ const App = () => {
       url: newUrl
     }
 
-    const createBlog = await blogService.create(blogObject)
-    setBlogs(blogs.concat(createBlog))
+    const createdBlog = await blogService.create(blogObject)
+    setAddMessage({ text: `a new blog ${createdBlog.title} by ${createdBlog.author} added`, type: 'success' })
+    setTimeout(() => {
+      setAddMessage({ text: null, type: '' })
+    }, 5000)
+    setBlogs(blogs.concat(createdBlog))
     setNewTitle('')
     setNewAuthor('')
     setNewUrl('')
@@ -70,6 +96,8 @@ const App = () => {
     return (
       <div>
         <h2>Log in to application</h2>
+        <Notification message={loginMessage} />
+
         <form onSubmit={handleLogin}>
           <div>
             username
@@ -98,6 +126,8 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
+      <Notification message={addMessage} />
+
       <p>
         {user.username} logged in
         <button onClick={handleLogout}>
