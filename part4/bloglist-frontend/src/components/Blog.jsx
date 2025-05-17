6c002df1c5a54ab4d3e8450849807e0a67ba899e
Nov4ou <nov4ou@outlook.com>
2025-05-17 22:20:30 +0800

    feat: complete exercise 5.7

--- part4/bloglist-frontend/src/components/Blog.jsx
+++ part4/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,40 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [expanded, setExpanded] = useState(false)
+
+  const toggleExpanded = () => {
+    setExpanded(!expanded)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title}
+        <button onClick={toggleExpanded}>
+          {expanded ? 'hide' : 'view'}
+        </button>
+      </div>
+
+      {expanded && (
+        <div>
+          <div>{blog.url}</div>
+          <div>
+            likes {blog.likes} <button>like</button>
+          </div>
+          <div>{blog.author}</div>
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Blog
