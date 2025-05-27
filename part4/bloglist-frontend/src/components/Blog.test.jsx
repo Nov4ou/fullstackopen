88789daad37d8a2b824c8a2c3cf7866ab87fc502
Nov4ou <nov4ou@outlook.com>
2025-05-27 17:25:53 +0800

    feat: complete exercise 5.16

--- part4/bloglist-frontend/src/components/Blog.test.jsx
+++ part4/bloglist-frontend/src/components/Blog.test.jsx
@@ -2,6 +2,7 @@ import { render, screen } from '@testing-library/react'
 import Blog from './Blog'
 import userEvent from '@testing-library/user-event'
 import { expect } from 'vitest'
+import BlogForm from './BlogForm'
 
 test('renders title and author, but not url or likes', () => {
   const blog = {
@@ -64,4 +65,28 @@ test('if like button is clicked twice, handler is called twice', async () => {
   await user.click(likeButton)
   await user.click(likeButton)
   expect(mockLikeHandler.mock.calls).toHaveLength(2)
+})
+
+test('<BlogForm /> calls createBlog with right details', async () => {
+  const createBlog = vi.fn()
+  const user = userEvent.setup()
+
+  render(<BlogForm createBlog={createBlog} />)
+
+  const inputs = screen.getAllByRole('textbox')
+  // screen.debug(input)
+  const [titleInput, authorInput, urlInput] = inputs
+  const createButton = screen.getByText('create')
+
+  await user.type(titleInput, 'test a form title')
+  await user.type(authorInput, 'test a form author')
+  await user.type(urlInput, 'test a form url')
+  await user.click(createButton)
+
+  expect(createBlog.mock.calls).toHaveLength(1)
+  expect(createBlog.mock.calls[0][0]).toEqual({
+    title: 'test a form title',
+    author: 'test a form author',
+    url: 'test a form url'
+  })
 })
