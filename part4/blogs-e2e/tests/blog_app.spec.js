1fa027c1a5083f47cb051cafb9080053be74c600
Nov4ou <nov4ou@outlook.com>
2025-05-29 19:23:40 +0800

    fix: rewrite exercise 5.18 using "Controlling the state of the database"

--- part4/blogs-e2e/tests/blog_app.spec.js
+++ part4/blogs-e2e/tests/blog_app.spec.js
@@ -1,7 +1,16 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:3003/api/testing/reset')
+    await request.post('http://localhost:3003/api/users', {
+      data: {
+        name: 'e2e-test',
+        username: 'e2e-test',
+        password: 'test'
+      }
+    })
+
     await page.goto('http://localhost:5173')
   })
 
@@ -12,17 +21,17 @@ describe('Blog app', () => {
   describe('Login', () => {
     test('succeeds with correct credentials', async ({ page }) => {
       await page.getByRole('button', { name: 'log in' }).click()
-      await page.getByTestId('username').fill('passwd_is_passwd')
-      await page.getByTestId('password').fill('passwd')
+      await page.getByTestId('username').fill('e2e-test')
+      await page.getByTestId('password').fill('test')
       await page.getByRole('button', { name: 'login' }).click()
 
-      await expect(page.getByText('passwd_is_passwd logged in')).toBeVisible()
+      await expect(page.getByText('e2e-test logged in')).toBeVisible()
     })
 
     test('fails with wrong credentials', async ({ page }) => {
       await page.getByRole('button', { name: 'log in' }).click()
-      await page.getByTestId('username').fill('passwd_is_passwd')
-      await page.getByTestId('password').fill('password')
+      await page.getByTestId('username').fill('e2e-test')
+      await page.getByTestId('password').fill('e2e-test')
       await page.getByRole('button', { name: 'login' }).click()
 
       await expect(page.getByText('wrong username or password')).toBeVisible()
